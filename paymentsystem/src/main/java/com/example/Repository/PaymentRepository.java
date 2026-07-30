91cf3588707a136a3719dc25ce13ab44b503e00f
yadavreddyrohitha <yadavareddyrohitha@gmail.com>
2026-07-30 08:41:00 +0000

    created payment model

---
+++ paymentsystem/src/main/java/com/example/Repository/PaymentRepository.java
@@ -0,0 +1,12 @@
+package com.example.paymentsystem.repository;
+
+
+import com.example.paymentsystem.model.Payment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface PaymentRepository 
+extends JpaRepository<Payment,Long>{
+
+}
---
+++ paymentsystem/src/main/java/com/example/Service/PaymentService.java
@@ -0,0 +1,61 @@
+package com.example.paymentsystem.service;
+
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+import org.springframework.stereotype.Service;
+
+import com.example.paymentsystem.model.Payment;
+import com.example.paymentsystem.repository.PaymentRepository;
+
+
+
+@Service
+public class PaymentService {
+
+
+private final PaymentRepository repository;
+
+
+public PaymentService(PaymentRepository repository){
+
+this.repository=repository;
+
+}
+
+
+
+public Payment createPayment(Payment payment){
+
+
+payment.setStatus("CREATED");
+
+payment.setCreatedAt(LocalDateTime.now());
+
+
+return repository.save(payment);
+
+}
+
+
+
+
+public List<Payment> getPayments(){
+
+return repository.findAll();
+
+}
+
+
+
+public Payment getPayment(Long id){
+
+return repository.findById(id)
+.orElse(null);
+
+}
+
+
+}
